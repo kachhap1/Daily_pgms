bc5ffd2958c5db6a9dcd7ce6acd745b39361cd28
aARTi <114593308+kachhap1@users.noreply.github.com>
2024-07-03 23:04:12 +0530

    Create 41_quickSort.java

---
+++ 41_quickSort.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class quicksort {
+    public static void print(int arr[]){
+        for(int i =0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static  void qsort(int arr[], int si, int ei){
+
+        if(si>=ei){
+            return;
+        }
+        int pidx = partition(arr,si,ei);
+        qsort(arr,si,pidx-1); //left
+        qsort(arr,pidx+1,ei); //right
+    }
+    public static int partition(int arr[],int si , int ei){
+        int pivot = arr[ei];    //taking pivot as last element
+
+        int i = si-1;
+        for(int j = si ; j<ei ;j++)
+        {
+            if(arr[j] <= pivot)
+            {
+                i++;
+                //swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp ;
+            }
+        }
+        i++;
+        int temp = pivot;
+        arr[ei] = arr[i];
+        arr[i] = temp;
+
+        return i;
+    }
+
+    public static void main(String args[]){
+        int arr[]={6,3,9,8,2,5};
+        qsort(arr,0,arr.length-1);
+        print(arr);
+    }
+}
